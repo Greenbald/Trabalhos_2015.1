4bb046011ee9c13927ceebcbe2ad63acf60afa69
Paulo Roberto Mann Marques Júnior <paulomann@id.uff.br>
2015-04-13 19:03:53 -0300

    You can now pin the address in the map.
    inputs are now available, the algorithm is still pending.
    Inputs now show properly the marks in the map.

--- IA/calculate.js
+++ IA/calculate.js
@@ -1,16 +1,50 @@
 function calculatePath(place_a, place_b, algorithm)
-{
-	
+{	
+	var latlngs = [];
+	$.getJSON('http://nominatim.openstreetmap.org/search/' + place_a + '?format=json&addressdetails=1&limit=1&polygon_svg=1', function(placea)
+	{
+		if(placea.length == 0)
+			alert(place_a + ' could not be found.');
+		else
+			latlngs.push([placea[0].lat, placea[0].lon]);
+		$.getJSON('http://nominatim.openstreetmap.org/search/' + place_b + '?format=json&addressdetails=1&limit=1&polygon_svg=1', function(placeb)
+		{
+			if(placeb.length == 0)
+				alert(place_b + ' could not be found.');
+			else if(placeb.length > 0 && placea.length > 0)
+			{
+				latlngs.push([placeb[0].lat, placeb[0].lon]);
+				addmark(latlngs[0][0], latlngs[0][1], "");
+				addmark(latlngs[1][0], latlngs[1][1], "");
+			}
+		});
+	});
 }
 
-function drawLine(place_a, place_b)
+function drawLine(latlngs)
 {
-	
+	var polyline = L.polyline(latlngs, {color: 'red'}).addTo(map);
+	map.fitBounds(polyline.getBounds());
 }
-
+var marks = [];
+var canDeleteMarks = false;
 function addmark(lat, lng, text)
 {
-	L.marker([lat, lng]).addTo(map)
-    .bindPopup(text)
-    .openPopup();
+	if(canDeleteMarks)
+	{
+			for(var i = 0; i < marks.length; i++)
+				map.removeLayer(marks[i]);
+			canDeleteMarks = false;
+			marks = [];
+	}
+	var actualMark = new L.marker([lat,lng]);
+	marks.push(actualMark);
+	map.addLayer(actualMark);
+	actualMark.bindPopup(text).openPopup();
+	if(marks.length == 2)
+		canDeleteMarks = true;
+	
+	//L.marker([lat, lng]).addTo(map)
+    //.bindPopup(text)
+    //.openPopup();
 }
--- IA/leafletembed.js
+++ IA/leafletembed.js
@@ -10,10 +10,10 @@ function initmap() {
 	// create the tile layer with correct attribution
 	var osmUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
 	var osmAttrib='Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors';
-	var osm = new L.TileLayer(osmUrl, {minZoom: 8, maxZoom: 14, attribution: osmAttrib});		
+	var osm = new L.TileLayer(osmUrl, {minZoom: 8, maxZoom: 30, attribution: osmAttrib});		
 
 	// start the map in Niteroi
-	map.setView(new L.LatLng(-22.8992863, -43.0842731),9);
+	map.setView(new L.LatLng(-22.8992863, -43.0842731),13);
 	map.addLayer(osm);
 	
 }
